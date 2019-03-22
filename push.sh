3096aa20577973984dd7704bc94e1714ac67699c
learningNicolas <q641259875@outlook.com>
2019-03-22 20:53:17 +0800

    $msg

---
+++ push.sh
@@ -0,0 +1,15 @@
+# git 脚本
+
+if [ $1 != '']
+then
+    msg=$1
+else
+    msg='msg is null';
+fi
+
+echo "执行git add 命令"
+git add .
+echo "执行git commit 命令"
+git commit -m '$msg'
+echo "执行git push 命令"
+git push origin master
